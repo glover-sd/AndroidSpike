7c74c66cce7590d3513465894f30250be5bdda22
glover-sd <brent.l.glover@gmail.com>
2021-09-04 08:32:32 -0700

    Still having issues resolving some resource dependencies in TodoAdapter.kt, so will not build

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,15 +3,39 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        var btn :Button = TODO()
 
+        todoAdapter = TodoAdapter(mutableListOf())
+        val rvTodoItems: RecyclerView = findViewById(R.id.rvTodoItems)
+        val btnAddTodo: Button = findViewById(R.id.btnAddTodo)
+        val etTodoTitle: EditText = findViewById(R.id.etTodoTitle)
+        val btnDeleteDone: Button = findViewById(R.id.btnDeleteDone)
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDone.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 
 
---
+++ app/src/main/java/com/example/helloworld/Todo.kt
@@ -0,0 +1,6 @@
+package com.example.helloworld
+
+data class Todo(
+    val title: String,
+    var isChecked: Boolean = false
+)
